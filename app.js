3b00a07e68e93f5ac6c0c6f79f03b65c54b2933f
antmihlin <antmihlin@gmail.com>
2018-12-03 17:11:34 +0100

    emit on a new message

--- app.js
+++ app.js
@@ -3,21 +3,53 @@ const http = require("http");
 const socketIo = require("socket.io");
 const axios = require("axios");
 const port = process.env.PORT || 4001;
-const index = require("./routes/index");
+//const index = require("./routes/index");
 const app = express();
-app.use(index);
+const cors = require('cors');
+
+app.use(cors()); // Use this after the variable declaration
+
+//app.use(index);
 const server = http.createServer(app);
 const io = socketIo(server, {
 	origins: 'http://localhost:3006'
 });
+
+global.messages = [];
+
+//______________________________________________________________________
+
+//const express = require("express");
+const router = express.Router();
+router.get("/", (req, res) => {
+  res.set('Access-Control-Allow-Origin', '*');
+  res.set('Access-Control-Allow-Credentials', 'false');
+  res.send({ response: "I am alive" }).status(200);
+});
+
+router.post("/", (req, res) => {
+  res.set({
+	  'Access-Control-Allow-Origin': 'http://localhost:3006', 
+	  'Access-Control-Allow-Credentials': 'false'
+  });
+  global.messages.push(req.query);
+  getApiAndEmit(socketConnection);
+  res.send(req.query).status(200);
+});
+
+app.use(router);
+
+//______________________________________________________________________
+
+let socketConnection = null;
+
 io.on("connection", socket => {
-  console.log("New client connected"), setInterval(
-    () => getApiAndEmit(socket),
-    10000
-  );
-  socket.on("disconnect", () => console.log("Client disconnected"));
+	socketConnection = socket;
+	socket.on("disconnect", () => console.log("Client disconnected"));
 });
 const getApiAndEmit = async socket => {
-    socket.emit("FromAPI");
+    socket.emit("FromAPI",global.messages);
 };
 server.listen(port, () => console.log(`Listening on port ${port}`));
+
+
