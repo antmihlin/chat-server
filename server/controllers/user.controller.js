5591f50c406e0532bdc4134415de8607c21f654d
Anton <antmihlin@gmail.com>
2019-04-08 22:20:41 +0200

    User fix

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -8,7 +8,6 @@ const User = require('../models/user.model');
 
 const passport = require('passport');
 const jwt = require('jsonwebtoken');
-require('../config/passport');
 
 /*------------------------------------------------------------*/
 
